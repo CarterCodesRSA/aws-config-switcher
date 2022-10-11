0797d423ac966232ec408d048003cdda980618af
Kyle Hoffman <kyle.hoffman@orderin.co.za>
2022-10-11 14:34:51 +0200

    Set aws configure commands

--- src/main.py
+++ src/main.py
@@ -9,23 +9,24 @@ def get_profile_credentials()-> dict:
     config.read(c.AWS_CONFIG_FILE_PATH)
 
     selected_profile = h.show_input_choices(config.sections())
+    print(f"Setting keys to {selected_profile}'s profile")
 
     credentials = {
         "key_id": config.get(selected_profile, 'aws_access_key_id'),
-        "secret_id": config.get(selected_profile, 'aws_access_key_id')
+        "secret_id": config.get(selected_profile, 'aws_secret_access_key')
     }
 
     return credentials
 
-def run_aws_configure(aws_key: str, aws_secret: str):
-    subprocess.run(f"aws configure set aws_access_key_id {aws_key}", stderr=subprocess.PIPE, shell=True, check=True, text=True)
-    subprocess.run(f"aws configure set aws_secret_access_key {aws_secret}", stderr=subprocess.PIPE, shell=True, check=True, text=True)
+
+def run_aws_configure(key_id: str, secret_id: str):
+    subprocess.run(f"aws configure set aws_access_key_id {key_id}", stderr=subprocess.PIPE, shell=True, check=True, text=True)
+    subprocess.run(f"aws configure set aws_secret_access_key {secret_id}", stderr=subprocess.PIPE, shell=True, check=True, text=True)
 
 
 def main():
     credentials = get_profile_credentials()
-    print(credentials)
-    
+    run_aws_configure(credentials["key_id"], credentials["secret_id"])
 
 if __name__ == "__main__":
     main()
